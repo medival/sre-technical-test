78c53d9f6fb9a2974cb8b590252845afb6b712a8
Adi Purnomo <dev.adipurnomo@gmail.com>
2022-08-06 23:30:01 +0700

    tf: deploy gke cluster

---
+++ provision-gke-cluster/main.tf
@@ -0,0 +1,83 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.55.0"
+    }
+  }
+}
+
+provider "google" {
+  project     = var.project_id
+  region      = var.region
+  zone        = var.zone
+}
+
+module "gke_auth" {
+  source       = "terraform-google-modules/kubernetes-engine/google//modules/auth"
+  depends_on   = [module.gke]
+  project_id   = var.project_id
+  location     = module.gke.location
+  cluster_name = module.gke.name
+}
+
+resource "local_file" "kubeconfig" {
+  content  = module.gke_auth.kubeconfig_raw
+  filename = "kubeconfig-${var.env_name}"
+}
+
+# module "gcp-network" {
+#   source       = "terraform-google-modules/network/google"
+#   project_id   = var.project_id
+#   network_name = "${var.network}-${var.env_name}"
+#   subnets = [
+#     {
+#       subnet_name   = "${var.subnetwork}-${var.env_name}"
+#       subnet_ip     = "10.10.0.0/16"
+#       subnet_region = var.region
+#     },
+#   ]
+#   secondary_ranges = {
+#     "${var.subnetwork}-${var.env_name}" = [
+#       {
+#         range_name    = var.ip_range_pods_name
+#         ip_cidr_range = "10.20.0.0/16"
+#       },
+#       {
+#         range_name    = var.ip_range_services_name
+#         ip_cidr_range = "10.30.0.0/16"
+#       },
+#     ]
+#   }
+# }
+
+module "gke" {
+  source            = "terraform-google-modules/kubernetes-engine/google//modules/private-cluster"
+  project_id        = var.project_id
+  name              = "${var.cluster_name}-${var.env_name}"
+  regional          = false
+  region            = var.region
+  zones             = var.zone
+  # network           = "default"
+  # subnetwork        = "default"
+  ip_range_pods     = var.ip_range_pods_name
+  ip_range_services = var.ip_range_services_name
+  node_pools = [
+    {
+      name           = "node-pool"
+      machine_type   = "n2-standard-2"
+      node_locations = "asia-southeast2-a"
+      min_count      = var.minnode
+      max_count      = var.maxnode
+      disk_size_gb   = var.disksize
+      preemptible    = true
+      auto_repair    = false
+      auto_upgrade   = false
+    },
+  ]
+}
+
+output "cluster_name" {
+  description = "Cluster name"
+  value       = module.gke.name
+}
---
+++ provision-gke-cluster/variables.tf
@@ -0,0 +1,59 @@
+variable "project_id" {
+  description = "The project ID of your project"
+  deafult     = "submission-adi-purnomo"
+}
+variable "cluster_name" {
+  description = "The name for the GKE cluster"
+  default     = "metaverse"
+}
+variable "env_name" {
+  description = "The environment for the GKE cluster"
+  default     = "dev"
+}
+variable "region" {
+  description = "The region to host the cluster in"
+  default     = "asia-southeast2"
+}
+variable "zones" {
+  description = "Cluster zone"
+  default     = "asia-southeast2-a"
+}
+variable "network" {
+  description = "The VPC network created to host the cluster in"
+  # default     = "gke-network"
+}
+variable "subnetwork" {
+  description = "The subnetwork created to host the cluster in"
+  # default     = "gke-subnet"
+}
+variable "ip_range_pods_name" {
+  description = "The secondary ip range to use for pods"
+  default     = "ip-range-pods"
+}
+variable "ip_range_services_name" {
+  description = "The secondary ip range to use for services"
+  default     = "ip-range-services"
+}
+
+variable "service-account-id" {
+  description = "The ID of service account of GCP"
+  default     = "serviceaccount-id"
+}
+variable "cpus" {
+  description = "Number of cpus"
+  default     = "2"
+}
+
+variable "minnode" {
+  description = "Minimum number of node pool"
+  default     = 1
+}
+variable "maxnode" {
+  description = "Maximum number of node pool"
+  default     = 2
+}
+
+variable "disksize" {
+  description = "Disk Size in GB"
+  default     = 15
+}

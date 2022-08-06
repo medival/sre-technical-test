a07a0584676590b0564fb35ab7fc2510d2e75a62
Adi Purnomo <dev.adipurnomo@gmail.com>
2022-08-06 21:01:59 +0700

    update: startup script

--- provisioning-infrastructure/modules/instances/startup_script.sh
+++ provisioning-infrastructure/modules/instances/startup_script.sh
@@ -4,5 +4,5 @@ sudo apt-get update
 sudo apt install software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
-sudo curl -O /etc/ssh/ https://gist.githubusercontent.com/medival/82819390ee218076bcb08db852b271b2/raw/150463bed8e8b94bbf387455937cce7d5c0a71d0/sshd_config
+sudo curl -O /etc/ssh/ https://gist.githubusercontent.com/medival/82819390ee218076bcb08db852b271b2/raw/35f28b1f2ea4b027645b082648d3f0d79001352c/sshd_config
 sudo systemctl restart ssh

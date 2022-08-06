88b0f96db4d37929d4f8677c077b90311e9501a4
Adi Purnomo <dev.adipurnomo@gmail.com>
2022-08-06 23:48:58 +0700

    tf: fix typo

--- provision-gke-cluster/variables.tf
+++ provision-gke-cluster/variables.tf
@@ -1,6 +1,6 @@
 variable "project_id" {
   description = "The project ID of your project"
-  deafult     = "submission-adi-purnomo"
+  default     = "submission-adi-purnomo"
 }
 variable "cluster_name" {
   description = "The name for the GKE cluster"
